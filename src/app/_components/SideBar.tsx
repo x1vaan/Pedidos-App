4a501c1058fef03dabc932ae64c2b0b53ad2692c
x1vaan <ivanx467@gmail.com>
2024-02-17 03:30:32 -0300

    homepage creating sidebar

---
+++ src/app/_components/SideBar.tsx
@@ -0,0 +1,7 @@
+export default function SideBar(){
+    return (
+        <aside className="w-[250px] mt-10 h-[100vh] overflow-auto">
+            
+        </aside>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@nextui-org/button";
+import SideBar from "./_components/SideBar";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <div className="flex flex-row">
+        <SideBar />
+        <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+          Abrir Caja
+        </Button>
+      </div>
     </main>
   );
 }
